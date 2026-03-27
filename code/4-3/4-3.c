f2e0205d1c6edf145a17f8a80ecc3da224f55011
bible <biblely0217@gmail.com>
2026-03-27 09:45:02 +0900

    4-3

--- code/4-3/4-3.c
+++ code/4-3/4-3.c
@@ -4,7 +4,7 @@ int main() {
     int arr[5] = {1, 2, 3, 4, 5};
 
     for (int i = 0; i < 5; i++) {
-        printf("&arr[%d] = %p\n", i, &arr[i]);
+        printf("%p\n", &arr[i]);
     }
 
     return 0;
