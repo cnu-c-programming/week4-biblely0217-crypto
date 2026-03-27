540aa7a2b90d9fb524e141c7a8c9c2835293951e
bible <biblely0217@gmail.com>
2026-03-27 09:30:18 +0900

    solve 2-2

--- code/2-3/2-3.c
+++ code/2-3/2-3.c
@@ -6,17 +6,14 @@ int main() {
     char str2[100] = "World";
     char str3[100];
 
-    // 1. 문자열 복사
     strcpy(str3, str1);
+    printf("%s\n", str3);
 
-    // 2. 문자열 길이
     printf("%lu\n", strlen(str1));
 
-    // 3. 문자열 이어붙이기
     strcat(str1, str2);
     printf("%s\n", str1);
 
-    // 4. 문자열 비교
     printf("%d\n", strcmp(str1, str2));
 
     return 0;
