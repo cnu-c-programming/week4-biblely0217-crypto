bceccf885578537aa6b49b50138aebdc2cc6e278
bible <biblely0217@gmail.com>
2026-03-27 10:02:02 +0900

    4-3

--- code/4-3/4-3.c
+++ code/4-3/4-3.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int arr[5] = {1, 2, 3, 4, 5};
+    int arr[] = {3, 7, 1, 9, 4, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-    for (int i = 0; i < 5; i++) {
-        printf("%p\n", &arr[i]);
+    for(int i = 0; i < (n - 1); i++) {
+        void* addr_0 = &(arr[i]);
+        void* addr_1 = &(arr[i+1]);
+
+        printf("address of arr[%d]: %p\n", i, addr_0);
+        printf("address of arr[%d]: %p\n", i+1, addr_1);
+
+        printf("&(arr[%d]) - &(arr[%d]) = %lld\n",
+               i, i+1,
+               (unsigned long long)addr_1 - (unsigned long long)addr_0);
+
+        printf("\n");
     }
 
     return 0;
